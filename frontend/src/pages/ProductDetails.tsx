48316fb899045b0640ad59c978c541f0d3ee86da
abulKhasim <abulkhasim999@gmail.com>
2025-07-13 20:27:46 +0530

    Input Validation in Product Detail page

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -43,8 +43,8 @@ const ProductDetails = () => {
                         </div>
 
                         {/* Product Title and Description */}
-                        <div className="flex flex-col justify-between gap-2 lg:gap-8">
-                            <div className="flex items-start justify-between">
+                        <div className="w-full flex flex-col justify-between gap-2 lg:gap-8">
+                            <div className="w-full flex items-start justify-between">
                                 <div className="flex flex-col gap-1">
                                     <h2 className="text-xl text-[#505050] leading-none">{product.category}</h2>
                                     <h1 className="text-[2.25rem] font-medium leading-none text-purple-700">{product.name}</h1>
@@ -60,24 +60,33 @@ const ProductDetails = () => {
                     <div className="w-full flex flex-col lg:flex-row items-start justify-between gap-6 lg:gap-10">
                         <div className="lg:w-[70%]">
                             {/* About Section */}
-                            <div className="mt-10">
-                                <h3 className="text-2xl mb-3">About {product.name}</h3>
-                                <ul className="text-lg list-disc space-y-1 px-8 sm:px-10">
-                                    {product.aboutPoints?.map((point: string, i: number) => (
-                                        <li key={i}>{point}</li>
-                                    ))}
-                                </ul>
-                            </div>
+                            {
+                                product.aboutPoints?.filter((a: any) => a.trim() !== '').length > 0 && (
+                                    <div className="mt-10">
+                                        <h3 className="text-2xl mb-3">About {product.name}</h3>
+                                        <ul className="text-lg list-disc space-y-1 px-8 sm:px-10">
+                                            {product.aboutPoints?.map((point: string, i: number) => (
+                                                <li key={i}>{point}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                )
+                            }
 
                             {/* Benefits Section */}
-                            <div className="mt-10">
-                                <h3 className="text-2xl mb-3">Benefits</h3>
-                                <ul className="text-lg list-disc space-y-1 px-8 sm:px-10">
-                                    {product.benefitPoints?.map((benefit: string, i: number) => (
-                                        <li key={i}>{benefit}</li>
-                                    ))}
-                                </ul>
-                            </div>
+                            {
+                                product.benefitPoints?.filter((a: any) => a.trim() !== '').length > 0 && (
+                                    <div className="mt-10">
+                                        <h3 className="text-2xl mb-3">Benefits</h3>
+                                        <ul className="text-lg list-disc space-y-1 px-8 sm:px-10">
+                                            {product.benefitPoints?.map((benefit: string, i: number) => (
+                                                <li key={i}>{benefit}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                )
+                            }
+
                         </div>
 
                         {/* Right Sidebar Details */}
@@ -86,33 +95,46 @@ const ProductDetails = () => {
                                 Last updated: {new Date(product.lastUpdated).toLocaleString()}
                             </p>
 
-                            <div className="bg-[#F0F0F0] p-3 rounded-lg">
-                                <p className="text-[#505050] font-light leading-none pb-2">Active Ingredients</p>
-                                <p className="text-xl ">
-                                    {product.activeIngredients?.join(", ") || "N/A"}
-                                </p>
-                            </div>
+                            {
+                                product.activeIngredients?.filter((a: any) => a.trim() !== '').length > 0 && (
+                                    <div className="bg-[#F0F0F0] p-3 rounded-lg">
+                                        <p className="text-[#505050] font-light leading-none pb-2">Active Ingredients</p>
+                                        <p className="text-xl ">
+                                            {product.activeIngredients?.join(", ") || "N/A"}
+                                        </p>
+                                    </div>
+                                )
 
-                            <div className="bg-[#F0F0F0] p-3 rounded-lg">
-                                <p className="text-[#505050] font-light leading-none pb-2">Formulation Type</p>
-                                <p className="text-xl ">
-                                    {product.formulationTypes?.join(", ") || "N/A"}
-                                </p>
-                            </div>
+                            }
 
-                            <div className="bg-[#F0F0F0] p-4 rounded-lg">
-                                <p className="text-[#505050] font-light leading-none pb-3">Crops</p>
-                                <div className="flex flex-wrap gap-2">
-                                    {product.crops?.map((crop: string) => (
-                                        <span
-                                            key={crop}
-                                            className="bg-purple-200 text-purple-800 px-3 py-1 rounded-full text-xl"
-                                        >
-                                            {crop}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
+                            {
+                                product.formulationTypes?.filter((a: any) => a.trim() !== '').length > 0 && (
+                                    <div className="bg-[#F0F0F0] p-3 rounded-lg">
+                                        <p className="text-[#505050] font-light leading-none pb-2">Formulation Type</p>
+                                        <p className="text-xl ">
+                                            {product.formulationTypes?.join(", ") || "N/A"}
+                                        </p>
+                                    </div>
+                                )
+                            }
+
+                            {
+                                product.crops?.filter((a: any) => a.trim() !== '').length > 0 && (
+                                    <div className="bg-[#F0F0F0] p-4 rounded-lg">
+                                        <p className="text-[#505050] font-light leading-none pb-3">Crops</p>
+                                        <div className="flex flex-wrap gap-2">
+                                            {product.crops?.map((crop: string) => (
+                                                <span
+                                                    key={crop}
+                                                    className="bg-purple-200 text-purple-800 px-3 py-1 rounded-full text-xl"
+                                                >
+                                                    {crop}
+                                                </span>
+                                            ))}
+                                        </div>
+                                    </div>
+                                )
+                            }
                         </div>
                     </div>
                 </div>
