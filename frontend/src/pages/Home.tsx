009907c62f5902423a11c3e6306aa6716282ce07
abulKhasim <abulkhasim999@gmail.com>
2025-07-26 21:49:50 +0530

    Removed old section and redesing new sections

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,23 @@
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
-import FranchiseOpportunities from "../sections/FranchiseOpportunities"
-import KakudAgriMission from "../sections/KakudAgriMission"
+import MoreAboutUs from "../sections/MoreAboutUs"
+// import FranchiseOpportunities from "../sections/FranchiseOpportunities"
+import OurProducts from "../sections/OurProducts"
 // import KeyServices from "../sections/KeyServices"
-import OurMission from "../sections/OurMission"
-import OurServices from "../sections/OurServices"
+// import OurServices from "../sections/OurServices"
 import Testimonials from "../sections/Testimonials"
-import WhyKakud from "../sections/WhyKakud"
+// import WhyKakud from "../sections/WhyKakud"
 
 const Home = () => {
     return (
         <div>
             <Hero />
-            <OurMission />
-            <OurServices />
-            <KakudAgriMission />
-            <FranchiseOpportunities />
+            {/* <OurServices /> */}
+            <OurProducts />
+            <MoreAboutUs />
+            {/* <FranchiseOpportunities /> */}
             {/* <KeyServices /> */}
-            <WhyKakud />
+            {/* <WhyKakud /> */}
             <Testimonials />
             <Footer />
         </div>
