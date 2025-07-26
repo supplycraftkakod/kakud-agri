051108545d33c3fa89480dd6eee57d5ec0dd35f6
abulKhasim <abulkhasim999@gmail.com>
2025-07-26 21:48:17 +0530

    image in testimonial

--- frontend/src/sections/Testimonials.tsx
+++ frontend/src/sections/Testimonials.tsx
@@ -1,35 +1,8 @@
-// import leftArow from "../assets/icons/left-arrow.png"
-// import rihgtArow from "../assets/icons/right-arrow.png"
-
-// const Testimonials = () => {
-//     return (
-//         <div className="w-full py-[2rem] sm:py-[3.5rem] font-inter bg-red-400">
-//             <div className="w-full py-16 sm:h-[27rem] px-6 xs:px-[1rem] md:px-[3rem] lg:px-[3rem] xl:px-[6rem] flex items-center justify-between bg-[#F6F6F6]">
-//                 <img src={leftArow} alt="left" className="cursor-pointer" />
-
-//                 {/* here */}
-//                 <div className="px-6 sm:px-[5rem] lg:px-[10rem] xl:px-[15rem] flex flex-col items-center gap-8 bg-green-700">
-//                     <div>
-//                         <h3 className="text-2xl sm:text-[2rem] text-center leading-none">Anil Deshmukh</h3>
-//                         <h4 className="text-xl sm:text-2xl text-[#505050] text-center">Karnataka</h4>
-//                     </div>
-//                     <p className="sm:text-xl text-center">
-//                         “Partnering with Kakud Agri as a franchisee has been a game-changer for my business. The exclusive product range and support from the team have ensured steady growth.”
-//                     </p>
-//                 </div>
-
-//                 <img src={rihgtArow} alt="right" className="cursor-pointer" />
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Testimonials
-
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import leftArrow from "../assets/icons/left-arrow.png";
 import rightArrow from "../assets/icons/right-arrow.png";
+import leafBgImg from '../assets/images/leaf-bg.png'
 
 const testimonials = [
     {
@@ -84,8 +57,13 @@ const Testimonials = () => {
     };
 
     return (
-        <div className="w-full pt-[2rem] sm:pt-[3.5rem] font-inter">
-            <div className="w-full py-36 sm:h-[32rem] px-6 xs:px-[1rem] md:px-[3rem] lg:px-[3rem] xl:px-[6rem] flex items-center justify-between bg-[#e2e2e2] overflow-hidden">
+        <div className="w-full font-inter">
+            <div className="w-full py-40 sm:h-[28rem] mb-10 px-6 xs:px-[1rem] md:px-[3rem] lg:px-[3rem] xl:px-[6rem] flex items-center justify-between bg-[#ffffff] bg-cover overflow-hidden"
+                style={{
+                    backgroundImage: `url(${leafBgImg})`,
+                    backgroundPosition: "center",
+                }}
+            >
                 <img
                     src={leftArrow}
                     alt="left"
@@ -93,7 +71,8 @@ const Testimonials = () => {
                     onClick={handlePrev}
                 />
 
-                <div className="relative w-full flex justify-center items-center">
+                <div className="relative w-full flex justify-center items-center"
+                >
                     <AnimatePresence custom={direction} mode="wait">
                         <motion.div
                             key={index}
