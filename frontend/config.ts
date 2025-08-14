00dd983c820d5040402dc0a2f55c5a5325c69dc9
abulKhasim <abulkhasim999@gmail.com>
2025-08-14 12:11:29 +0530

    Added company credentials

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,8 +1,11 @@
-export const BE_URL = "https://kakud.onrender.com"
-export const FE_URL = "https://kakud-agri.vercel.app"
+export const BE_URL = "https://kakud-agri.onrender.com"
+export const FE_URL = "https://kakud.in"
 
+
+// https://kakud-agri.onrender.com
 // https://kakud.onrender.com
 // http://localhost:5000
 
+// https://kakud.in
 // https://kakud-agri.vercel.app
 // http://localhost:5173
